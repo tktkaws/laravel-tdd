4f1c2804740c314b6b815c1bbfbc9327d979960c
takayuki-takahashi-dic <ttkaws15@gmail.com>
2020-08-18 11:09:52 +0900

    feature test

---
+++ app/app/Http/Controllers/LessonController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Lesson;
+use App\Models\VacancyLevel;
+use Illuminate\Http\Request;
+
+class LessonController extends Controller
+{
+    public function show(Lesson $lesson)
+    {
+        $vacancyLevel = new VacancyLevel(0);
+        return view('lesson.show', compact('lesson'));
+
+    }
+}
--- app/routes/web.php
+++ app/routes/web.php
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/lessons/{lesson}', 'LessonController@show')->name('lessons.show');
