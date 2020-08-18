d3e30a7986c4e3470a56a2b3b282acae251946b1
takayuki-takahashi-dic <ttkaws15@gmail.com>
2020-08-18 12:37:51 +0900

    unit test / feature test

--- app/app/Models/VacancyLevel.php
+++ app/app/Models/VacancyLevel.php
@@ -16,14 +16,36 @@ class VacancyLevel
     return $this->mark();
 }
 
+//     public function mark(): string
+// {
+//     if ($this->remainingCount === 0) {
+//         return '×';
+//     }
+//     if ($this->remainingCount < 5) {
+//         return '△';
+//     }
+//     return '◎';
+// }
+
+    public function slug(): string
+    {
+        if ($this->remainingCount === 0) {
+            return 'empty';
+        }
+        if ($this->remainingCount < 5) {
+            return 'few';
+        }
+        return 'enough';
+    }
+
     public function mark(): string
-{
-    if ($this->remainingCount === 0) {
-        return '×';
+    {
+        $marks = ['empty' => '×', 'few' => '△', 'enough' => '◎'];
+        $slug = $this->slug();
+        assert(isset($marks[$slug]), new \DomainException('invalid slug value.'));
+
+        return $marks[$slug];
     }
-    if ($this->remainingCount < 5) {
-        return '△';
-    }
-    return '◎';
-}
+
+
 }
--- app/tests/Unit/Models/VacancyLevelTest.php
+++ app/tests/Unit/Models/VacancyLevelTest.php
@@ -7,6 +7,35 @@ use App\Models\VacancyLevel;
 
 class VacancyLevelTest extends TestCase
 {
+     /**
+     * @param int $remainingCount
+     * @param string $expectedSlug
+     * @dataProvider dataSlug
+     */
+    public function testSlug(int $remainingCount, string $expectedSlug)
+    {
+        $level = new VacancyLevel($remainingCount);
+        $this->assertSame($expectedSlug, $level->slug());
+    }
+
+    public function dataSlug()
+    {
+        return [
+            '空きなし' => [
+                'remainingCount' => 0,
+                'expectedSlug' => 'empty',
+            ],
+            '残りわずか' => [
+                'remainingCount' => 4,
+                'expectedSlug' => 'few',
+            ],
+            '空き十分' => [
+                'remainingCount' => 5,
+                'expectedSlug' => 'enough',
+            ],
+        ];
+    }
+
       /**
      * @param int $remainingCount
      * @param string $expectedMark
